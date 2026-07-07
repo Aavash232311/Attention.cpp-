bd40653a37a4bee4bfa57d9ddca52811214b943c
avash <aavash2005@gmail.com>
2026-07-06 21:16:13 -0500

    Added: reading dimension based on config json dumped by C++

--- unit_test/kernel_debugger.py
+++ unit_test/kernel_debugger.py
@@ -12,15 +12,27 @@ os.system("nvcc -DDEBUG src/attention.cpp src/kernel/math.cu -o src/bin/attentio
 os.system("./src/bin/attention")
 
 
-
+import json
 import torch
 import warnings
 
 warnings.filterwarnings("ignore", category=UserWarning, message="The given buffer is not writable")
 
+import sys
+
+try:
+    with open('./src/cache/config.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
+        
+except (FileNotFoundError, json.JSONDecodeError) as e:
+    print(f"Critical Error: {e}")
+    print("Exiting program.")
+    sys.exit(1)  
+
 with open("./src/cache/token_embeddings.bin", "rb") as f:
     buf = f.read()
 
-token_embeddings = torch.frombuffer(buf, dtype=torch.float32).reshape(32, 892)
+token_embeddings = torch.frombuffer(buf, dtype=torch.float32).reshape(data['d_model'], data['vocab_size'])
+
 
 print(token_embeddings)
