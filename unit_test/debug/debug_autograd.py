Fixed: wrong order of relase of token embedding

unit_test/debug/debug_autograd.py
@@ -1,6 +1,6 @@
-import os
 import torch
 
+from debug.static import RED, GREEN, RESET
 from binary_reader.autograd_binary_reader import Reader
 
 def debug_autograd(
@@ -10,10 +10,6 @@ def debug_autograd(
         vocab_size: int,
         num_heads: int
 ):
-    # Loads C++ with python generated parameters to avoid randomness
-    os.system(
-        "nvcc -DDEBUG -DDRUN src/main.cpp src/kernel/utils.cu src/forward/Kernel/layer_norm.cu src/forward/Kernel/embedding.cu src/forward/Kernel/linear.cu src/forward/Kernel/attention_head.cu src/forward/Kernel/interface.cu src/backpropagation/Kernel/interface_back.cu -o src/bin/attention")
-    os.system("./src/bin/attention")
 
     delta, y_predicted, y_actual, h, dl_dw_kernel, h_t, wt, w = Reader(
         batch_size=batch_size,
@@ -37,5 +33,16 @@ def debug_autograd(
 
     # Note-: GIGO sometimes you might just be transposing the garbage who knows
     # if you think that is the case then manually print and see from the C++ script.
-    print(f"Checking delta across kernels {torch.allclose(delta_torch, delta)}")
-    print(f"Checking wt transpose kernel: {torch.allclose(w.T, wt)}")
+
+    check_delta_across_kernels = torch.allclose(delta_torch, delta)
+    if not check_delta_across_kernels:
+        print(f"Checking delta across kernels: {RED} {check_delta_across_kernels} {RESET}")
+    else:
+        print(f"Checking delta across kernels: {GREEN} {check_delta_across_kernels} {RESET}")
+
+    check_wt = torch.allclose(w.T, wt)
+
+    if not check_wt:
+        print(f"Checking wt transpose kernel: {RED} {torch.allclose(w.T, wt)} {RESET}")
+    else:
+        print(f"Checking wt transpose kernel: {GREEN} {check_wt} {RESET}")

unit_test/debug/debug_embeddings.py
@@ -1,34 +1,19 @@
 import torch
 
-
-from ml_components.transformer import Transformer
+from debug.static import RED, GREEN, RESET
 from binary_reader.embedding_reader import Debugger
-from ml_components.normal_dis import  Predictability
+from ml_components.transformer import total_embeddings
 from ml_components.positional_encoding import sinusoidal_positional_encoding
-
-
 
 def verify_embeddings(
         d_model: int,
         seq_len: int,
         batch_size: int,
         vocab_size: int,
-        num_heads: int
+        num_heads: int,
+        token_embeddings
 ):
-    model = Transformer(
-        batch_size,
-        seq_len,
-        d_model,
-        vocab_size,
-        num_heads
-    )
-
-    predictability = Predictability(
-        d_model=d_model,
-        vocab_size=vocab_size,
-        seq_len=seq_len,
-        batch_size=batch_size
-    )
+    torch.set_printoptions(sci_mode=False, precision=4)
 
     debugger = Debugger(
         C=d_model,
@@ -39,18 +24,16 @@ def verify_embeddings(
         folder='./src/cache/cpp_out'
     )
 
-    token_embeddings = predictability.token_embeddings(
-        d_model,
-        vocab_size
-    )
-
     pe = sinusoidal_positional_encoding(seq_len=seq_len, d_model=d_model)
     # same embedding that c++ uses after being released from python.
     k_total_embeddings = debugger.readEmbeddings('embedding.bin', batch_size * seq_len * d_model)
     x = debugger.readX("x.bin", seq_len * batch_size)
 
-    py_total_embedding = model.total_embeddings(x=x, token_embedding_table=token_embeddings,
+    py_total_embedding = total_embeddings(x=x, token_embedding_table=token_embeddings,
                                                 positional_embedding_table=pe)
 
-
-    print(f"Checking net embedding C++ kernel, status: {torch.allclose(py_total_embedding, k_total_embeddings)}")
+    check = torch.allclose(py_total_embedding, k_total_embeddings)
+    if not check:
+        print(f"Checking net embedding C++ kernel, status:{RED} {check} {RESET}")
+        return
+    print(f"Checking net embedding C++ kernel, status:{GREEN} {check} {RESET}")

unit_test/kernel_debugger.py
@@ -2,6 +2,7 @@ import os
 import torch
 from pathlib import Path
 
+from rnd.release_embeddings import release_token_embeddings
 from debug.debug_autograd import debug_autograd
 from debug.debug_embeddings import verify_embeddings
 from binary_reader.hyperparamaters import read_hyperparamaters
@@ -14,13 +15,19 @@ os.chdir(Path.cwd().parent)
 # Read release hyperparameters, compiles the C++ and releases the hyperparamaters.
 d_model, vocab_size, batch_size, seq_len, num_heads = read_hyperparamaters()
 
+token_emb = release_token_embeddings(d_model=d_model, seq_len=seq_len, vocab_size=vocab_size, batch_size=batch_size)
+
+# After hyperparamaters are loaded, we need to load those binary that the C++ releases
+os.system("nvcc -DDEBUG -DDRUN src/main.cpp src/kernel/utils.cu src/forward/Kernel/layer_norm.cu src/forward/Kernel/embedding.cu src/forward/Kernel/linear.cu src/forward/Kernel/attention_head.cu src/forward/Kernel/interface.cu src/backpropagation/Kernel/interface_back.cu -o src/bin/attention")
+os.system("./src/bin/attention")
+
 print("\n")
 print('*' * 60)
 print("Forward pass ")
 print('*' * 60)
 
 # check if embedding component of the transformer is okay.
-verify_embeddings(d_model=d_model, seq_len=seq_len, batch_size=batch_size, vocab_size=vocab_size, num_heads=num_heads)
+verify_embeddings(d_model=d_model, seq_len=seq_len, batch_size=batch_size, vocab_size=vocab_size, num_heads=num_heads, token_embeddings=token_emb)
 
 torch.set_printoptions(precision=4)
 

unit_test/ml_components/transformer.py
@@ -5,6 +5,18 @@ import torch.nn as nn
 '''
     Transformer for sanity check of the cuda kernels.
 '''
+
+
+def total_embeddings(x, token_embedding_table, positional_embedding_table):
+    T, B = x.shape
+
+    tok_emb = token_embedding_table[x]               # (T, B, C)
+    pos_emb = positional_embedding_table[:T]          # (T, C)
+
+    out = tok_emb + pos_emb.unsqueeze(1)               # (T, B, C)
+    return out.transpose(0, 1)                          # (B, T, C)
+
+
 class Transformer(nn.Module):
 
     def __init__(self, batch_size, seq_len, d_model, vocab_size, num_heads):
@@ -18,15 +30,5 @@ class Transformer(nn.Module):
         self.device = torch.device("cpu")
         if torch.cuda.is_available():
             self.device = torch.device("cuda")
-
-
-    def total_embeddings(self, x, token_embedding_table, positional_embedding_table):
-        T, B = x.shape
-
-        tok_emb = token_embedding_table[x]               # (T, B, C)
-        pos_emb = positional_embedding_table[:T]          # (T, C)
-
-        out = tok_emb + pos_emb.unsqueeze(1)               # (T, B, C)
-        return out.transpose(0, 1)                          # (B, T, C)
 
 