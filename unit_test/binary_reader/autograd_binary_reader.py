Added: binary release of Qk after fixcing the wrong order of matrix multiplication

unit_test/binary_reader/autograd_binary_reader.py
@@ -61,7 +61,12 @@ def ReaderFlashAttention(
                         dtype=np.float32
                     ).to(device)
 
-    return P, V, PT, VT, G_unc, dl_dh, dp, dV, softmax_upstream, dQ, k, q, d_score_t
+    dK = load_tensor("./src/cache/cpp_out/dk.bin",
+                     shape=(batch_size, num_heads, seq_len, head_dim),
+                     dtype=np.float32
+                     ).to(device)
+
+    return P, V, PT, VT, G_unc, dl_dh, dp, dV, softmax_upstream, dQ, k, q, d_score_t, dK
 
 def Reader(
         batch_size: int,

unit_test/debug/debug_flash_attention.py
@@ -16,7 +16,7 @@ class DebugFlashAttention(torch.nn.Module):
         self.dl_dw = dl_dw # Upstream gradient G
 
         # looks like ideal gas equation, but it's not
-        self.P, self.V, self.PT, self.VT, self.G_unc, self.G, self.dp, self.dv, self.softmax_upstream, self.dQ, self.K, self.Q, self.d_score_t = ReaderFlashAttention(batch_size, seq_len, vocab_size, d_model, num_heads, head_dim)
+        self.P, self.V, self.PT, self.VT, self.G_unc, self.G, self.dp, self.dv, self.softmax_upstream, self.dQ, self.K, self.Q, self.d_score_t, self.dK = ReaderFlashAttention(batch_size, seq_len, vocab_size, d_model, num_heads, head_dim)
 
     # dV = P^T G
     # dP = GV^T
@@ -83,6 +83,7 @@ class DebugFlashAttention(torch.nn.Module):
         check_dq = torch.allclose(dQ, self.dQ, atol=1e-4, rtol=1e-4)
 
         check_d_score_t = torch.allclose(d_score_torch.transpose(2, 3), self.d_score_t, atol=1e-4, rtol=1e-4)
+
 
         if not check_dp:
             print(f"Checking dp kernel, status:{RED} {check_dp} {RESET}")