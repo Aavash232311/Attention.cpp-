Fixed: netupstream gradient g x hat for layer norm kernel

unit_test/debug/debug_flash_attention.py
@@ -171,12 +171,12 @@ class DebugFlashAttention(torch.nn.Module):
 
         # Now lets check the contact of these qkv weights
 
-        # dQ_Wq = self.upq @ self.wq.T
-        # dK_Wk = self.upk @ self.wk.T
-        # dV_Wv = self.upv @ self.wv.T
+        dQ_Wq = self.upq @ self.wq.T
+        dK_Wk = self.upk @ self.wk.T
+        dV_Wv = self.upv @ self.wv.T
 
-        # G_hat_net = dQ_Wq + dK_Wk + dV_Wv
-        # print(torch.allclose(G_hat_net, self.G_x_hat,atol=1e-4, rtol=1e-4))
+        G_hat_net = dQ_Wq + dK_Wk + dV_Wv
+        print(torch.allclose(G_hat_net, self.G_x_hat,atol=1e-4, rtol=1e-4))
         # print(G_hat_net)
         # print(self.G_x_hat)
         # print(self.upk.shape)